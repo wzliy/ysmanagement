1fbe566df8bdc94b10233c9f5603641c933c3c4a
Arvin <Arvin@jiaozhentech.com>
2018-08-02 16:38:47 +0800

    first commit init project

---
+++ src/main/java/com/arvin/ysmanagement/YsmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.arvin.ysmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YsmanagementApplication.class, args);
+	}
+}
---
+++ src/test/java/com/arvin/ysmanagement/YsmanagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arvin.ysmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YsmanagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
